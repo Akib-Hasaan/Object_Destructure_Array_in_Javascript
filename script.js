078b25c574fece946e42f30ba6d2efdc117e184e
Akib-Hasaan <akibhasan582@gmail.com>
2024-11-11 22:25:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// (1) Object
+
+const desherNam = {
+    deshNumber1 : "Bangladesh",
+    deshNumber2 : "Canada"
+}
+console.log(desherNam);
+
+
+// (2) Object Destructure
+
+var {jatiNumber1,jatiNumber2} = {
+    jatiNumber1 : ["Bangla Jati", 100],
+    jatiNumber2 : "English Jati"
+}
+console.log(jatiNumber1);
+
+// Note = console.log(typeof jatiNumber1); shows us that this is an object
+// Note = Object can be used by any type of variable 
+
+// (3) Arrays
+// Note =
+ var khabar = ["alu", "piaj", "morich", 100]
+// Note = console.log(typeof khabar); shows us that this is an object
+// Note = Array is used by the "[]" sign and allows us to use multiple variables or select individual variables
+// Note = Array can be used by any type of variable
